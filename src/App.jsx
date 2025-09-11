4361d7b3019fb44e2685c3035267dedeee4e1ccb
Damian Davies <127567007+Damian-Dvs@users.noreply.github.com>
2025-09-11 07:41:02 +0100

    added vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Analytics } from '@vercel/analytics/react'
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Services from "./components/Services";
@@ -18,6 +19,8 @@ function App() {
       <Testimonials />
       <ContactForm />
       <Footer />
+      {/* Vercel Analytics */}
+      <Analytics />
     </>
   );
 }
